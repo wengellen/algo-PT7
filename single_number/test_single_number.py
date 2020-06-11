40e267d7074c6d430d0d481c7e19e7180dda4bb4
Yilun Weng <wengellen@gmail.com>
2020-06-10 21:58:52 -0700

    Finish single number

--- single_number/test_single_number.py
+++ single_number/test_single_number.py
@@ -1,20 +1,34 @@
 import unittest
 import random
-from single_number import single_number
+from single_number import single_number, single_number_pointers,   single_number_pointers_sorted
 
 class Test(unittest.TestCase):
     def test_single_number(self):
         arr = []
 
-        for i in range(1000):
+        for i in range(10000):
+            arr.append(i)
+            arr.append(i)
+        random.shuffle(arr)
+        arr.sort()
+        rand_index = random.randint(0, len(arr))
+        num = arr.pop(rand_index)
+
+
+        self.assertEqual(single_number(arr), num)
+
+    def test_single_number_pointers(self):
+        arr = []
+        for i in range(10000):
             arr.append(i)
             arr.append(i)
 
         random.shuffle(arr)
+        arr.sort()
         rand_index = random.randint(0, len(arr))
         num = arr.pop(rand_index)
 
-        self.assertEqual(single_number(arr), num)
+        self.assertEqual(single_number_pointers(arr), num)
 
 
 if __name__ == '__main__':
